bdd3094abc263ee783fe5fc961624e5aa579b735
Tina Wang <63619055+xiaossr@users.noreply.github.com>
2023-04-06 20:09:53 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import requests
+from requests_oauthlib import OAuth1
+import os
+import random
+
+consumer_key = os.environ.get("CONSUMER_KEY")
+consumer_secret = os.environ.get("CONSUMER_SECRET")
+access_token = os.environ.get("ACCESS_TOKEN")
+access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
+
+allquotes = list(open('quotes.txt'))
+
+def random_quote():
+  return random.choice(allquotes).rstrip()
+
+def format_quote(quote):
+  return {"text": "{}".format(quote)}
+
+def connect_to_oauth(consumer_key, consumer_secret, access_token, access_token_secret):
+  url = "https://api.twitter.com/2/tweets"
+  auth = OAuth1(consumer_key, consumer_secret, acccess_token, access_token_secret)
+  return url, auth
+
+def hello_pubsub(event, context):
+  fact = random_fact()
+  payload = format_fact(fact)
+  url, auth = connect_to_oauth(
+      consumer_key, consumer_secret, access_token, access_token_secret
+  )
+  request = requests.post(
+      auth=auth, url=url, json=payload, headers={"Content-Type": "application/json"}
+  )
